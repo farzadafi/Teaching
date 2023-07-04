cb5b7b25fd29a7773b36936392231f529ce4b06b
farzadafi <farzadafi40@gmail.com>
2023-07-04 15:17:10 +0330

    remove apples list and use form Inventory class

--- Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/a_normal/NormalMain.java
+++ Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/a_normal/NormalMain.java
@@ -1,35 +1,24 @@
 package main.java.a_normal;
 
-import main.java.base.Apple;
-import main.java.base.Colors;
-
-import java.util.Arrays;
-import java.util.List;
+import main.java.base.Inventory;
 
 public class NormalMain {
     public static void main(String[] args) {
         Farmer farmer = new Farmer();
-        List<Apple> apples = Arrays.asList(
-                new Apple(Colors.YELLOW, 200D, "Iran"),
-                new Apple(Colors.RED, 100D, "Iran"),
-                new Apple(Colors.GREEN, 250D, "Iran"),
-                new Apple(Colors.YELLOW, 100D, "Iran"),
-                new Apple(Colors.GREEN, 50D, "Iran")
-        );
 
         System.out.println("Green:");
-        System.out.println(farmer.filterGreenApple(apples));
+        System.out.println(farmer.filterGreenApple(Inventory.APPLE_LIST));
 
         System.out.println("Yellow:");
-        System.out.println(farmer.filterYellowApple(apples));
+        System.out.println(farmer.filterYellowApple(Inventory.APPLE_LIST));
 
         System.out.println("Red:");
-        System.out.println(farmer.filterRedApple(apples));
+        System.out.println(farmer.filterRedApple(Inventory.APPLE_LIST));
 
         System.out.println("Heavy:");
-        System.out.println(farmer.filterHeavyApple(apples));
+        System.out.println(farmer.filterHeavyApple(Inventory.APPLE_LIST));
 
         System.out.println("light:");
-        System.out.println(farmer.filterLightApple(apples));
+        System.out.println(farmer.filterLightApple(Inventory.APPLE_LIST));
     }
 }
