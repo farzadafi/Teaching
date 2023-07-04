9b0f1f450bbe43382233255e6b8126df34dda2e7
farzadafi <farzadafi40@gmail.com>
2023-07-04 15:17:56 +0330

    remove apples list and use form Inventory class

--- Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/b_perdicate_class/PredicateClassMain.java
+++ Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/b_perdicate_class/PredicateClassMain.java
@@ -6,11 +6,7 @@ import main.java.b_perdicate_class.predicate.color.AppleRedColorPredicate;
 import main.java.b_perdicate_class.predicate.color.AppleYellowColorPredicate;
 import main.java.b_perdicate_class.predicate.weight.AppleHeavyWeightPredicate;
 import main.java.b_perdicate_class.predicate.weight.AppleLightWeightPredicate;
-import main.java.base.Apple;
-import main.java.base.Colors;
-
-import java.util.Arrays;
-import java.util.List;
+import main.java.base.Inventory;
 
 public class PredicateClassMain {
     public static void main(String[] args) {
@@ -21,27 +17,19 @@ public class PredicateClassMain {
         ApplePredicate heavyApple = new AppleHeavyWeightPredicate();
         ApplePredicate lightApple = new AppleLightWeightPredicate();
 
-        List<Apple> apples = Arrays.asList(
-                new Apple(Colors.YELLOW, 200D, "Iran"),
-                new Apple(Colors.RED, 100D, "Iran"),
-                new Apple(Colors.GREEN, 250D, "Iran"),
-                new Apple(Colors.YELLOW, 100D, "Iran"),
-                new Apple(Colors.GREEN, 50D, "Iran")
-        );
-
         System.out.println("Green:");
-        System.out.println(predicateFarmerClass.filtersApple(apples, greenApple));
+        System.out.println(predicateFarmerClass.filtersApple(Inventory.APPLE_LIST, greenApple));
 
         System.out.println("Yellow:");
-        System.out.println(predicateFarmerClass.filtersApple(apples, yellowApple));
+        System.out.println(predicateFarmerClass.filtersApple(Inventory.APPLE_LIST, yellowApple));
 
         System.out.println("Red:");
-        System.out.println(predicateFarmerClass.filtersApple(apples, redApple));
+        System.out.println(predicateFarmerClass.filtersApple(Inventory.APPLE_LIST, redApple));
 
         System.out.println("Heavy:");
-        System.out.println(predicateFarmerClass.filtersApple(apples, heavyApple));
+        System.out.println(predicateFarmerClass.filtersApple(Inventory.APPLE_LIST, heavyApple));
 
         System.out.println("light:");
-        System.out.println(predicateFarmerClass.filtersApple(apples, lightApple));
+        System.out.println(predicateFarmerClass.filtersApple(Inventory.APPLE_LIST, lightApple));
     }
 }
