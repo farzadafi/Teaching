b2fce83a48a14abeddef6725d15a231732c951d3
farzadafi <farzadafi40@gmail.com>
2023-07-04 15:25:19 +0330

    remove apples list and use form Inventory class

--- Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/c_anonymous_class/AnonymousClassMain.java
+++ Java/Java8/Lambda/code/BehaviorParameterization/src/main/java/c_anonymous_class/AnonymousClassMain.java
@@ -2,24 +2,15 @@ package main.java.c_anonymous_class;
 
 import main.java.base.Apple;
 import main.java.base.Colors;
+import main.java.base.Inventory;
 import main.java.c_anonymous_class.predicate.ApplePredicate;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class AnonymousClassMain {
     public static void main(String[] args) {
         PredicateFarmerAnonymousClass predicateFarmerAnonymousClass = new PredicateFarmerAnonymousClass();
-        List<Apple> apples = Arrays.asList(
-                new Apple(Colors.YELLOW, 200D, "Iran"),
-                new Apple(Colors.RED, 100D, "Iran"),
-                new Apple(Colors.GREEN, 250D, "Iran"),
-                new Apple(Colors.YELLOW, 100D, "Iran"),
-                new Apple(Colors.GREEN, 50D, "Iran")
-        );
 
         System.out.println("Green:");
-        System.out.println(predicateFarmerAnonymousClass.filtersApple(apples, new ApplePredicate() {
+        System.out.println(predicateFarmerAnonymousClass.filtersApple(Inventory.APPLE_LIST, new ApplePredicate() {
             @Override
             public Boolean test(Apple apple) {
                 return apple.getColors().equals(Colors.GREEN);
@@ -27,7 +18,7 @@ public class AnonymousClassMain {
         }));
 
         System.out.println("Yellow:");
-        System.out.println(predicateFarmerAnonymousClass.filtersApple(apples, new ApplePredicate() {
+        System.out.println(predicateFarmerAnonymousClass.filtersApple(Inventory.APPLE_LIST, new ApplePredicate() {
             @Override
             public Boolean test(Apple apple) {
                 return apple.getColors().equals(Colors.YELLOW);
@@ -35,7 +26,7 @@ public class AnonymousClassMain {
         }));
 
         System.out.println("Red:");
-        System.out.println(predicateFarmerAnonymousClass.filtersApple(apples, new ApplePredicate() {
+        System.out.println(predicateFarmerAnonymousClass.filtersApple(Inventory.APPLE_LIST, new ApplePredicate() {
             @Override
             public Boolean test(Apple apple) {
                 return apple.getColors().equals(Colors.RED);
@@ -43,7 +34,7 @@ public class AnonymousClassMain {
         }));
 
         System.out.println("Heavy:");
-        System.out.println(predicateFarmerAnonymousClass.filtersApple(apples, new ApplePredicate() {
+        System.out.println(predicateFarmerAnonymousClass.filtersApple(Inventory.APPLE_LIST, new ApplePredicate() {
             @Override
             public Boolean test(Apple apple) {
                 return apple.getWeight() >= 150;
@@ -51,7 +42,7 @@ public class AnonymousClassMain {
         }));
 
         System.out.println("light:");
-        System.out.println(predicateFarmerAnonymousClass.filtersApple(apples, new ApplePredicate() {
+        System.out.println(predicateFarmerAnonymousClass.filtersApple(Inventory.APPLE_LIST, new ApplePredicate() {
             @Override
             public Boolean test(Apple apple) {
                 return apple.getWeight() < 150;
