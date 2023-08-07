d96b36557d115acc529a36426c219fd024aee4af
farzadafi <farzadafi40@gmail.com>
2023-08-07 15:24:43 +0330

    create User model

---
+++ Java/JDBC/code/JDBC/src/main/java/model/User.java
@@ -0,0 +1,63 @@
+package model;
+
+public class User {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String firstname, String lastname, String username, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
