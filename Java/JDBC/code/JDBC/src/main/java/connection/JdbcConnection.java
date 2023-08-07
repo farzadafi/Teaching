d342995f391637aa839da34031e33db41dc823c2
farzadafi <farzadafi40@gmail.com>
2023-08-07 10:24:31 +0330

    create JdbcConnection class for handle connection with singleton

---
+++ Java/JDBC/code/JDBC/src/main/java/connection/JdbcConnection.java
@@ -0,0 +1,42 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+    private Connection connection;
+    private static JdbcConnection obj;
+
+    private JdbcConnection() throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    "postgres",
+                    "afshar");
+        } catch (SQLException sql) {
+            System.out.println(sql.getMessage());
+        }
+    }
+
+    public static JdbcConnection getInstance() throws ClassNotFoundException {
+        if (obj == null) obj = new JdbcConnection();
+        return obj;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public static JdbcConnection getObj() {
+        return obj;
+    }
+
+    public static void setObj(JdbcConnection obj) {
+        JdbcConnection.obj = obj;
+    }
+}
