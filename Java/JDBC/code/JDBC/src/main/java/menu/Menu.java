b4e9b3bdd6b5bd0d0203c1d24d7cb81397753ffc
farzadafi <farzadafi40@gmail.com>
2023-08-08 09:45:00 +0330

    fix enterMenu method

--- Java/JDBC/code/JDBC/src/main/java/menu/Menu.java
+++ Java/JDBC/code/JDBC/src/main/java/menu/Menu.java
@@ -12,13 +12,12 @@ public class Menu {
     private final UserService userService = ApplicationContext.getUserService();
 
     public int publicMenu() {
-        int command;
         System.out.println("***WELCOME***");
         System.out.println("1-Sing in");
         System.out.println("2-Sing Up");
         System.out.println("3-Exit!");
         System.out.print("Please enter a number:");
-        command = input.nextInt();
+        int command = input.nextInt();
         input.nextLine();
         return switch (command) {
             case 1 -> 1;
@@ -36,8 +35,26 @@ public class Menu {
         User user = userService.login(username, password);
         if (user == null)
             System.out.println("You enter a wrong username or password");
-        else
-            System.out.println("Enter to your account!");
+        else {
+            boolean isTrue = true;
+            while (isTrue) {
+                System.out.println("==========================");
+                System.out.println("Dear " + user.getFirstname());
+                System.out.println("1-update my firstname");
+                System.out.println("2-delete my account");
+                System.out.println("3-Exit!");
+                System.out.print("Please enter a number:");
+                int command = input.nextInt();
+                input.nextLine();
+                switch (command) {
+                    case 1 -> userService.updateFirstname(user);
+                    case 2 -> System.out.println("naser");
+                    case 3 -> isTrue = false;
+                    default -> System.out.println("enter a correct number :)");
+                }
+                ;
+            }
+        }
     }
 
     public void registerMenu() throws SQLException {
